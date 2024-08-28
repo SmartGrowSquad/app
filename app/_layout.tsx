8b4669ce89980e9853f29b0aff71e3336b544eee
seonghyeoncho <chosh040@naver.com>
2024-08-29 00:50:09 +0900

    ✨ [feat] 폰트 로딩 로직 추가
    Resolves:
    Ref:
    Related:

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,6 @@
+import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
@@ -15,13 +17,36 @@ export {
 
 export const unstable_settings = {
   // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: '(home)',
+  initialRouteName: '(tabs)',
 };
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const [loaded, error] = useFonts({
+    PretendardBold: require('@/assets/fonts/Pretendard-Bold.ttf'), 
+    PretendardSemiBold: require('@/assets/fonts/Pretendard-SemiBold.ttf'), 
+    PretendardMedium: require('@/assets/fonts/Pretendard-Medium.ttf'), 
+    PretendardRegular: require('@/assets/fonts/Pretendard-Regular.ttf'), 
+    ...FontAwesome.font,
+  });
+
+  // Expo Router uses Error Boundaries to catch errors in the navigation tree.
+  useEffect(() => {
+    if (error) throw error;
+  }, [error]);
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
   return <RootLayoutNav />;
 }
 
